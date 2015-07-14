4f6b6efb0b91f2eeeb4f7110d4ec64d90eb99e53
SLICKnSWIFT <slicknswift25@gmail.com>
2015-07-13 21:51:45 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -109,7 +109,7 @@
         youtubeLink: "https://www.youtube.com/channel/UC-oWEWJDPAYpqqYVKWxSoGw",
         website: "http://arkhamnetwork.org/",
         intervalMessages: [],
-        messageInterval: 5,
+        messageInterval: 2,
         songstats: true,
         commandLiteral: "!",
         blacklists: {
